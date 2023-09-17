4a6e7a14109695244b776088bef8a03df13cbeed
Anton Tkachuk <tkacuk2291@gmail.com>
2023-09-17 20:16:06 +0300

    silutjdsh

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ def create_report(data_file_name: str,
                   report_file_name: str) -> None:
     supply = 0
     buy = 0
-    with open("test.txt", "r") as file_read:
+    with open(data_file_name, "r") as file_read:
         lines = file_read.readlines()
     for line in lines:
         action, amount = line.strip().split(",")
@@ -13,7 +13,7 @@ def create_report(data_file_name: str,
             buy += amount
     result = supply - buy
     print(f"sup , {supply},buy, {buy} ,result, {result}")
-    with open("test_name.txt", "w") as file_write:
+    with open(report_file_name, "w") as file_write:
         file_write.write(f"supply,{supply}\n")
         file_write.write(f"buy,{buy}\n")
         file_write.write(f"result,{result}\n")
